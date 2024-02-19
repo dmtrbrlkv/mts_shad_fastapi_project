8f7ee4af6e17edef22c3889fcc6e17652e9c6fc6
Dmitry Burlakov <dburlakov@mtsbank.ru>
2024-02-19 18:21:53 +0300

    алиас для email

--- src/schemas/sellers.py
+++ src/schemas/sellers.py
@@ -6,7 +6,10 @@ __all__ = ["IncomingSeller", "ReturnedSeller", "ReturnedAllSellers"]
 class BaseSeller(BaseModel):
     first_name: str = Field(min_length=3)
     last_name: str = Field(min_length=3)
-    e_mail: EmailStr
+    e_mail: EmailStr() = Field(alias="e-mail")
+
+    class Config:
+        populate_by_name = True
 
 
 # Класс для валидации входящих данных. Не содержит id так как его присваивает БД.
